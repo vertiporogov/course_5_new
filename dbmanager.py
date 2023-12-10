b2b1d4a1ef83f439ab119d8939e102b818d1ad41
alexander <vertiporogov.alexandr@mail.ru>
2023-12-10 22:09:54 +0300

    na segodni vse HHH

--- dbmanager.py
+++ dbmanager.py
@@ -12,37 +12,14 @@ class DBManager:
         conn = psycopg2.connect(dbname=self.db_name, **params)
 
         with conn.cursor() as cur:
-            # cur.execute("""
-            # SELECT * FROM company
-            # """)
-            # list_company = []
-            # list_vacancies = []
-            # rows = cur.fetchall()
-            # for row in rows:
-            #     list_company.append(row)
 
-            # for i in range(len(list_company)):
             cur.execute(f"""
-            SELECT company_name, company_id FROM vacancies
+            SELECT DISTINCT company_name FROM vacancies
             """)
             rows_1 = cur.fetchall()
-            count1 = 0
-            for i in rows_1:
-                if i[1] == 1:
-                    count1 += 1
-            # list_vacancies.append(rows_1)
-
-            # cur.execute("""
-            #             SELECT COUNT(*) FROM vacancies
-            #             WHERE company_id = 2
-            #             """)
-            # rows_2 = cur.fetchall()
 
         conn.commit()
         conn.close()
-        # print(rows_1)
-        # info_dict = {list_company[0][1]: list_vacancies[0][0][0], list_company[1][1]: list_vacancies[1][0][0],
-        #              list_company[2][1]: list_vacancies[2][0][0]}
 
         return rows_1
 
@@ -51,27 +28,3 @@ class DBManager:
     #     conn = psycopg2.connect(dbname=self.db_name, **params)
     #
     #     with conn.cursor() as cur:
-    #         cur.execute("""
-    #         SELECT * FROM company
-    #         """)
-    #         list_company = []
-    #         rows = cur.fetchall()
-    #         for row in rows:
-    #             list_company.append(row)
-    #
-    #         cur.execute("""
-    #         SELECT * FROM vacancies
-    #         WHERE company_id = 1
-    #         """)
-    #         rows_1 = cur.fetchall()
-    #
-    #         cur.execute("""
-    #                     SELECT * FROM vacancies
-    #                     WHERE company_id = 2
-    #                     """)
-    #         rows_2 = cur.fetchall()
-    #
-    #     conn.commit()
-    #     conn.close()
-
-    # info_list =
